96c37a375f772533ae8dc136e4d716c00bed6be4
yerinny <yerinlee90@gmail.com>
2020-09-07 13:20:09 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 
 //this is just creating temporary hats page
@@ -19,7 +20,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
